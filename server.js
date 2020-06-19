e2d2b53e06e7288716409c313e8b85e7eacd422a
Junior2Ran <hdr01@126.com>
2020-06-19 19:12:32 +0800

    fix bug

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ var url = require("url"),
             res.end("<h1>404 Not Found</h1>");
         }
     });
-}).listen(8080, "127.0.0.1");
+}).listen(8089, "127.0.0.1");
     
-console.log("Server running at http://127.0.0.1:8080/");
+console.log("Server running at http://127.0.0.1:8089/");
